168f8506f2732f52f64d9745a2909439be77e983
chrisdugne <chris.dugne@uralys.com>
2020-11-14 10:00:37 +0100

    prepared config

--- src/index.js
+++ src/index.js
@@ -2,6 +2,48 @@ import 'regenerator-runtime/runtime';
 import React from 'react';
 import {render} from 'react-dom';
 
+// -----------------------------------------------------------------------------
+
+const random1To = (n) => Math.floor(Math.random() * n) + 1;
+const randomColor = () => `#${random1To(9)}${random1To(9)}${random1To(9)}`;
+
+// -----------------------------------------------------------------------------
+
+const nbBuildings = 10;
+const nbLevelsPerBuilding = 20;
+const nbConnections = 10;
+const nbDevicesPerConnection = 20;
+
+const rangeBuildings = [...Array(nbBuildings).keys()];
+const rangeLevels = [...Array(nbLevelsPerBuilding).keys()];
+const rangeConnections = [...Array(nbConnections).keys()];
+const rangeDevices = [...Array(nbDevicesPerConnection).keys()];
+
+// -----------------------------------------------------------------------------
+
+const CONFIG = {
+  connections: rangeConnections.map((c) => ({
+    id: `connection-${c}`,
+    color: randomColor(),
+    devices: rangeDevices.map((d) => ({
+      id: `device-${c}-${d}`,
+      color: randomColor(),
+    })),
+  })),
+  buildings: rangeBuildings.map((b) => ({
+    id: `building-${b}`,
+    color: randomColor(),
+    levels: rangeLevels.map((l) => ({
+      id: `level-${b}-${l}`,
+      color: randomColor(),
+    })),
+  })),
+};
+
+console.log(CONFIG);
+
+// -----------------------------------------------------------------------------
+
 const App = (options = {}) => {
   if (!options.container) {
     console.error('[App] Requires a container.');
@@ -10,6 +52,8 @@ const App = (options = {}) => {
   const container = document.getElementById(options.container);
   render(<div>your app!</div>, container);
 };
+
+// -----------------------------------------------------------------------------
 
 if (window && !window.App) {
   window.App = App;
