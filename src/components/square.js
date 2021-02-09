ae543aa9910a15855fbb6e873fbb82b210f4a125
chrisdugne <chris.dugne@uralys.com>
2021-02-09 15:04:13 +0100

    displaying nbRenders on a Square

--- src/components/square.js
+++ src/components/square.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import styled, {css} from 'styled-components';
 
 const $Square = styled.div`
@@ -9,6 +9,15 @@ const $Square = styled.div`
   width: 100%;
   max-width: 50px;
   height: 50px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  p {
+    font-size: 18px;
+    font-weight: bold;
+  }
 
   ${props =>
     css`
@@ -24,6 +33,19 @@ const $Square = styled.div`
   transition: background-color 0.5s, border 0.2s;
 `;
 
-const Square = props => <$Square {...props} />;
+const Square = props => {
+  const [nbRenders, setNbRenders] = useState(0);
+  const {selected} = props;
+
+  useEffect(() => {
+    setNbRenders(nbRenders + 1);
+  }, [selected]);
+
+  return (
+    <$Square {...props}>
+      <p>{nbRenders}</p>
+    </$Square>
+  );
+};
 
 export default Square;
